257f2c1edea446c91eeda66f95139243b59c363d
PipAnd <pipchankaandrei@gmail.com>
2025-09-02 15:58:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/birthdaysaver/MainActivity.kt
+++ app/src/main/java/com/example/birthdaysaver/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.birthdaysaver
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.birthdaysaver.ui.add.AddGoalActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val btnAddGoal = findViewById<Button>(R.id.btnAddGoal)
+        btnAddGoal.setOnClickListener {
+            val intent = Intent(this, AddGoalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
